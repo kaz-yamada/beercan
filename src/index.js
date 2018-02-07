4836319a92e206b594ba78ef7ce1d2ba4a6d23ab
kaz-yamada <kaz.yamada@outlook.com>
2018-02-08 00:09:51 +1100

    Changed fonts

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
+// Load JavaScript
 // Import navigation scripts
 require( './js/navigation' );
-
 require( './js/app' );
-
 require( './js/skip-link-focus-fix' );
 
+// Load Stylesheets
 // import font awesome
 require( 'font-awesome/css/font-awesome.css' );
 
